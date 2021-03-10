00fc14eaf862edfe056ba2f9d0f0eeb8ba090a14
khushigour <khushigour0301@gmail.com>
2021-03-10 22:59:20 +0530

    using axios instead of fetch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react'
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -72,7 +73,7 @@ const App = () => {
     
     setStories({type: 'STORIES_FETCH_INIT'});
     
-   fetch(url).then(response => response.json()).then(result =>{
+   axios.get(url).then(result =>{
       setStories({
         type: 'STORIES_FETCH_SUCCESS',
         payload: result.hits
