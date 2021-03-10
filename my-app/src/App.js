3e48dcc320af21ead674a415d54edf16830becc7
khushigour <khushigour0301@gmail.com>
2021-03-10 20:05:39 +0530

    Conditional Rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,29 @@
 import './App.css';
 import React from 'react'
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]; 
+
+const getAsyncStories = () => new Promise(
+  resolve => setTimeout(() =>
+  resolve({data:  { stories: initialStories}}), 2000 )
+);
 
 //custom hook
 const usePState = (key, initialstate) =>{
@@ -13,39 +36,34 @@ const usePState = (key, initialstate) =>{
   }, [value]);
 
   return [value, setValue]
-}
+};
 
 
 const App = () => {
-
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ]
 
   const [searchTerm, setSearchTerm] = usePState(
    'search', 'React'
   );
 
   const [stories, setStories] = React.useState(initialStories);
+
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+
+  React.useEffect(()=>{
+     setIsLoading(true);
+    
+    getAsyncStories().then(result => {
+      setStories(result.data.stories);
+      setIsLoading(false);
+
+    }).catch(()=> setIsError(true));
+  }, []);
   
   const handleSearch = event => {
     setSearchTerm(event.target.value)
   }
+
 
 
   const handleStories = (item) => {
@@ -56,19 +74,29 @@ const App = () => {
   setStories(newStories);
   };
 
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>My Hacker Stories </h1>
       <Search 
-        id = "search"
-        value={searchTerm} 
-        onSearch={handleSearch}
-        isFocused
+          id = "search"
+          value={searchTerm} 
+          onSearch={handleSearch}
+          isFocused
       >
-        <strong>Search:</strong>
+          <strong>Search:</strong>
       </Search>
       <hr/>
-      <List stories={stories} onRemoveItem={handleStories} />
+      {isError && <p>Something went wrong...</p>}
+      {
+        isLoading? (<p>Loading...</p>): (
+          <List stories={searchedStories} onRemoveItem={handleStories} />
+        )
+      }
+      
 
     </div>
   );
@@ -103,7 +131,9 @@ const Item = ({item, onRemoveItem}) => (
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
       <span>
-        <button type="button" onClick={()=>onRemoveItem(item)}></button>
+        <button type="button" onClick={()=>onRemoveItem(item)}>
+          Dismiss
+        </button>
       </span>
     </div>
   );
